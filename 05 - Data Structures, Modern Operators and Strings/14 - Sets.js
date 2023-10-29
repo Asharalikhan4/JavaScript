3756bb7f05050751f1cf1e83765b3e4b5027e926
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-10-29 18:36:11 +0530

    Set added

---
+++ 05 - Data Structures, Modern Operators and Strings/14 - Sets.js
@@ -0,0 +1,40 @@
+/*
+
+Set objects are collections of values. A value in the set may only occur once; it is unique in the set's collection. You can iterate through the elements of a set in insertion order. The insertion order corresponds to the order in which each element was inserted into the set by the add() method successfully (that is, there wasn't an identical element already in the set when add() was called).
+
+The specification requires sets to be implemented "that, on average, provide access times that are sublinear on the number of elements in the collection". Therefore, it could be represented internally as a hash table (with O(1) lookup), a search tree (with O(log(N)) lookup), or any other data structure, as long as the complexity is better than O(N).
+
+Just like other's set also has some methods that we can use to make our work easy.
+
+*/
+
+// Implementation
+
+const mySet1 = new Set();
+
+mySet1.add(1); // Set(1) { 1 }
+mySet1.add(5); // Set(2) { 1, 5 }
+mySet1.add(5); // Set(2) { 1, 5 }
+mySet1.add("some text"); // Set(3) { 1, 5, 'some text' }
+const o = { a: 1, b: 2 };
+mySet1.add(o);
+
+mySet1.add({ a: 1, b: 2 }); // o is referencing a different object, so this is okay
+
+mySet1.has(1); // true
+mySet1.has(3); // false, since 3 has not been added to the set
+mySet1.has(5); // true
+mySet1.has(Math.sqrt(25)); // true
+mySet1.has("Some Text".toLowerCase()); // true
+mySet1.has(o); // true
+
+mySet1.size; // 5
+
+mySet1.delete(5); // removes 5 from the set
+mySet1.has(5); // false, 5 has been removed
+
+mySet1.size; // 4, since we just removed one value
+
+mySet1.add(5); // Set(5) { 1, 'some text', {...}, {...}, 5 } - a previously deleted item will be added as a new item, it will not retain its original position before deletion
+
+console.log(mySet1); // Set(5) { 1, "some text", {…}, {…}, 5 }
