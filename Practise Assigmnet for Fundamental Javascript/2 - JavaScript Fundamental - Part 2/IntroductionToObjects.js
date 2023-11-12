f6b6f98bfbf349b74dfd332104fdac4756427cdc
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-11-12 19:34:35 +0530

    Updated

--- Practise Assigmnet for Fundamental Javascript/2 - JavaScript Fundamental - Part 2/IntroductionToObjects.js
+++
@@ -1,7 +0,0 @@
-const myCountry = {
-    country: 'Finland',
-    capital: 'Helsinki',
-    language: 'finnish',
-    population: 6,
-    neighbours: ['Norway', 'Sweden', 'Russia']
-};
--- Practise Assigmnet for Fundamental Javascript/2 - JavaScript Fundamental - Part 2/LoopingBackwardsAndLoopsInLoops.js
+++
@@ -1,7 +0,0 @@
-const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
-
-for(let i = 0; i < listOfNeighbours.length; i++){
-    for(let k = 0; k < listOfNeighbours[i].length; k++){
-        console.log(`Neighbour ${listOfNeighbours[i][k]}`);
-    }
-}
