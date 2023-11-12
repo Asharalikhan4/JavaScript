889110882e094b49ad35b4d95389824b2645a2e1
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-11-12 22:26:46 +0530

    Updated

---
+++ 07 - Working with Arrays/10 - Some.js
@@ -0,0 +1,3 @@
+const movements =  [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+console.log(movements.includes(-130))
--- DSA/Basics/01 - Sum of all natural number from 1 to n.js
+++
@@ -1,5 +0,0 @@
-function sum(num){
-    return num*(num+1)/2;
-}
-
-console.log(sum(5));
