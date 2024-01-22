e14e46e3a20e3d3c38bd2e784f9b0c49e935b9b8
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-01-22 18:41:58 +0530

    Update

---
+++ 16 - Asynchronous JavaScript Promises, AsyncAwait, and AJAX/11 - Building a Promise.js
@@ -0,0 +1,64 @@
+// Building a Promise
+const lotteryPromise = new Promise(function(reject, resolve){
+    console.log("Lottery draw is happening");
+    setTimeout(() => {
+        if(Math.random() >= 0.5){
+            resolve("You Win")
+        } else {
+            reject(new Error("You Lost"));
+        }
+    }, 2000);
+});
+
+// Consuming a Promise
+lotteryPromise
+.then((res) => {
+    console.log(res);
+})
+.catch((err) => {
+    console.error(err);
+})
+
+
+// Promsifying setTimeout
+const wait = function(seconds){
+    // timer is never going to be rejected so no need of reject.
+    return new Promise(function(resolve){
+        setTimeout(resolve, seconds * 1000)
+    })
+}
+
+wait(1)
+.then(() => {
+    console.log("1 second passed");
+    return wait(1);
+})
+.then(() => {
+    console.log("2 second passed");
+    return wait(1);
+})
+.then(() => {
+    console.log("3 second passed");
+    return wait(1);
+})
+.then(() => {
+    console.log("4 second passed");
+})
+.catch((err) => {
+    console.error(err);
+})
+
+
+// Code above is same as below code but above code is more readable and clean.
+// setTimeout(() => {
+//     console.log("1 second passed");
+//     setTimeout(() => {
+//         console.log("2 second passed");
+//         setTimeout(() => {
+//             console.log("3 second passed");
+//             setTimeout(() => {
+//                 console.log("4 second passed");
+//             }, 1000)
+//         }, 1000)
+//     }, 1000)
+// }, 1000)
