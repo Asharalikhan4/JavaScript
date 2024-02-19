471ff58d41096bb8d047f648467d5061b8aa8a44
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-02-19 23:44:02 +0530

    update

---
+++ A to Z Striver DSA Sheet/01 Learn the basics/06 Hashing/02 - Hashing in Characters.js
@@ -0,0 +1,6 @@
+// The English alphabet consists of 26 characters, from 'a' to 'z'.
+
+// let chars = [a,a,a,a,b,,r,g,s,g,s,w,u,n,d,v,b,w,w,j,o,l,w,a,v,g];
+
+let hashArray = new Array(26).fill(0);
+
