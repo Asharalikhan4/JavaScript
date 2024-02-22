f8d0898d293df0ae8b3c7b5698781572c2f95927
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-02-23 00:25:02 +0530

    Code updated

--- A to Z Striver DSA Sheet/03 - Arrays/Medium/02 - Sort an array of 0s, 1s and 2s.js
+++ A to Z Striver DSA Sheet/03 - Arrays/Medium/02 - Sort an array of 0s, 1s and 2s.js
@@ -9,6 +9,30 @@ function sortArrayBrute(arr) {
 
 // Better
 function sortArrayBetter(arr) {
+    let zeroCount = 0, onesCount = 0, twoCount = 0;
+    for (let num of arr) {
+        if (num == 0) {
+            zeroCount++;
+        } else if (num == 1) {
+            onesCount++;
+        } else {
+            twoCount++;
+        }
+    }
+    for (let i = 0; i < onesCount; i++) {
+        arr[i] = 0;
+    }
+    for (let i = onesCount; i < zeroCount + onesCount; i++) {
+        arr[i] = 1;
+    }
+    for (let i = zeroCount + onesCount; i < arr.length; i++) {
+        arr[i] = 2;
+    }
+    return arr;
+}
+
+// Better
+function sortArrayOptimal(arr) {
     let start = 0, mid = 0, end = arr.length - 1;
     while (mid <= end) {
         if(arr[mid] == 0){
@@ -31,3 +55,4 @@ function sortArrayBetter(arr) {
 
 console.log("Answer (Brute) :", sortArrayBrute(arr));
 console.log("Answer (Better) :", sortArrayBetter(arr));
+console.log("Answer (OPtimal) :", sortArrayOptimal(arr));
