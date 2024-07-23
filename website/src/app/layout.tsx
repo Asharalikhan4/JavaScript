6e8cf5e32b9b1b98be246ef8507929ab7266b90a
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-07-23 13:06:40 +0530

    Dark theme added

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} container mx-auto p-4`}>
+      <body className={`${inter.className} container mx-auto p-4 dark:bg-black dark:text-white`}>
         <Navbar />
         {children}
       </body>
