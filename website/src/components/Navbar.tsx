4d00cc059a1ca963b39656d9506c99d291d2ddbc
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-07-26 16:38:02 +0530

    voilet color added in dark theme

--- website/src/components/Navbar.tsx
+++ website/src/components/Navbar.tsx
@@ -1,16 +1,18 @@
 import { FC } from "react";
 import Link from "next/link";
-import ThemeToggle from "@/utils/ThemeToggle";
+import ThemeToggle from "../utils/ThemeToggle";
 
 const Navbar: FC = () => {
     return (
-        <nav className="flex flex-shrink-0 justify-between items-center py-2 px-4 font-medium border-2 border-[#005B41] dark:border-cyan-500 rounded-md">
-            <Link href={"/"} className="cursor-pointer hover:text-[#005B41] hover:dark:text-cyan-100">Home</Link>
-            <div className="flex items-center gap-x-4">
-                <Link href={"/table-of-contents"} className="cursor-pointer hover:text-[#005B41] hover:dark:text-cyan-100">Contents</Link>
-                <ThemeToggle className="hover:text-[#005B41] hover:dark:text-cyan-100" />
-            </div>
-        </nav>
+        <div className="space-y-4">
+            <nav className="flex flex-shrink-0 justify-between items-center py-2 px-4 font-medium border-2 border-[#005B41] dark:border-violet-500 rounded-md">
+                <Link href={"/"} className="cursor-pointer transition-colors duration-200 hover:text-[#005B41] hover:dark:text-violet-300">Home</Link>
+                <div className="flex items-center gap-x-4">
+                    <Link href={"/table-of-contents"} className="cursor-pointer transition-colors duration-200 hover:text-[#005B41] hover:dark:text-violet-300">Contents</Link>
+                    <ThemeToggle className="transition-colors duration-200 hover:text-[#005B41] hover:dark:text-violet-300" />
+                </div>
+            </nav>
+        </div>
     );
 };
 
