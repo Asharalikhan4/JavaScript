579a57af71f760a021cb347a6c39edb923b74cd3
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-07-28 10:10:50 +0530

    Type fixes

--- website/src/utils/DynamicImport.tsx
+++ website/src/utils/DynamicImport.tsx
@@ -1,7 +1,7 @@
-import dynamic from "next/dynamic";
+import dynamic, { DynamicOptions, Loader } from "next/dynamic";
 import Loading from "@/app/loading";
 
-export function DynamicImport(importFunc) {
+export function DynamicImport(importFunc: DynamicOptions<{}> | Loader<{}>) {
   return dynamic(importFunc, {
     loading: () => <Loading />,
   });
