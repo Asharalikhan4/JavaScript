77649fdd3c8079ec8dad50aa564c282c2db4cfd4
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-09-05 08:02:04 +0530

    use state added

--- React Native/04-DivingDeeperIntoComponents/App.js
+++ React Native/04-DivingDeeperIntoComponents/App.js
@@ -1,18 +1,23 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from "expo-linear-gradient";
 import GameStartScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <LinearGradient colors={["#4e0329","#ddb52f"]} style={styles.rootScreen}>
-      <GameStartScreen />
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground source={require("")} resizeMode="cover" style={styles.rootScreen} imageStyle={styles.backgroundImage}>
+        <GameStartScreen />
+      </ImageBackground>
     </LinearGradient>
   );
-}
+};
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1, // taking as much space as availble.
     backgroundColor: "#ddb52f",
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
--- React Native/04-DivingDeeperIntoComponents/screens/StartGameScreen.js
+++ React Native/04-DivingDeeperIntoComponents/screens/StartGameScreen.js
@@ -1,16 +1,28 @@
 import { View, TextInput, StyleSheet } from "react-native";
+import { useState } from "react";
 import PrimaryButton from "../components/PrimaryButton";
 
 function GameStartScreen() {
+
+    const [enteredNumber, setEnteredNumber] = useState("");
+
+    function numberInputHandler(enteredNumber) {
+        setEnteredNumber(enteredNumber);
+    };
+
+    function confirmInputHandler() {
+
+    };
+
     return (
         <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false} />
+            <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false} value={enteredNumber} onChangeText={numberInputHandler} />
             <View style={styles.buttonsContainer}>
                 <View style={styles.buttonContainer}>
                     <PrimaryButton>Reset</PrimaryButton>
                 </View>
                 <View style={styles.buttonContainer}>
-                    <PrimaryButton>Confirm</PrimaryButton>
+                    <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
                 </View>
             </View>
         </View>
