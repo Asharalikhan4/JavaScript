79df186dc159b7534daac37d93d992e570196b71
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-09-06 21:57:28 +0530

    Guess logic handler added

--- React Native/04-DivingDeeperIntoComponents/App.js
+++ React Native/04-DivingDeeperIntoComponents/App.js
@@ -4,20 +4,35 @@ import { LinearGradient } from "expo-linear-gradient";
 import GameStartScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import Colors from './constants/colors';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
 
   const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setIsGameOver] = useState(true);
 
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
+    setIsGameOver(false);
+  };
+
+  function gameOverHandler() {
+    setIsGameOver(true);
+  };
+
+  let screen = <GameStartScreen onPickNumber={pickedNumberHandler} />;
+  if(userNumber) {
+    screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />;
+  };
+  if(gameIsOver && userNumber) {
+    screen = <GameOverScreen />
   };
 
   return (
     <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
       <ImageBackground source={require("")} resizeMode="cover" style={styles.rootScreen} imageStyle={styles.backgroundImage}>
         <SafeAreaView style={styles.rootScreen}>
-          userNumber ? <GameScreen /> : <GameStartScreen onPickNumber={pickedNumberHandler} />
+          {screen} 
         </SafeAreaView>
       </ImageBackground>
     </LinearGradient>
--- React Native/04-DivingDeeperIntoComponents/screens/GameScreen.js
+++ React Native/04-DivingDeeperIntoComponents/screens/GameScreen.js
@@ -1,12 +1,53 @@
-import { View, Text, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import { View, Text, StyleSheet, Alert } from "react-native";
+import { useState, useEffect } from "react-native";
+import Title from "../components/ui/Title";
+import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
-function GameScreen() {
+
+function generateRandomBetween(min, max, exclude) {
+    const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+    if (randomNumber === exclude) {
+        return generateRandomBetween(min, max, exclude);
+    } else {
+        return randomNumber;
+    }
+};
+
+let minBoundry = 1;
+let maxBoundry = 100;
+
+function GameScreen({ userNumber, onGameOver }) {
+
+    const initialGuess = generateRandomBetween(1, 100, userNumber);
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+    function nextGuessHandler(direction) {
+        if((direction === "lower" && currentGuess < userNumber) || (direction === "greater" && currentGuess > userNumber)) {
+            Alert.alert("Don't lie!", "You know that this is wrong...", [{ text: "Sorry!", style: "cancel"}]);
+            return;
+        }
+        direction === "lower" ? maxBoundry = currentGuess : minBoundry = currentGuess + 1;
+        const newRandomNumber = generateRandomBetween(minBoundry, maxBoundry, userNumber);
+        setCurrentGuess(newRandomNumber);
+    };
+
+    useEffect(() => {
+        if(currentGuess === userNumber){
+            onGameOver();
+        }
+    }, [currentGuess, userNumber, onGameOver]);
+
     return (
         <View style={styles.screen}>
             <Title>Opponent's Guess</Title>
+            <NumberContainer>{currentGuess}</NumberContainer>
             <View>
                 <Text>Higher or Lower</Text>
+                <View>
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>-</PrimaryButton>
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>+</PrimaryButton>
+                </View>
             </View>
             <View>
                 <Text>Log Rounds</Text>
