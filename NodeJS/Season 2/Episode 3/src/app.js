b9822528955a4cb0a840a77046e9ea6d88d56227
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-11-19 22:22:41 +0530

    Error handling

--- NodeJS/Season 2/Episode 3/src/app.js
+++ NodeJS/Season 2/Episode 3/src/app.js
@@ -9,6 +9,15 @@ app.use("/", (req, res) => {
     res.send("Hello From the Server.");
 });
 
+// handling the error
+app.use("/", (err, req, res, next) => {
+    try {
+        // write your logic here
+    } catch (err) {
+        res.status(500).send("Something went wrong.");
+    }
+})
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
