8bf7d49cbd082fe597f131e5a06ed0aff36fb28c
Asharalikhan4 <contactashar4@gmail.com>
2026-07-13 22:25:54 +0530

    Promise.all() polyfill added

--- JavaScript Interview Guide/1. Promise.all() Polyfill.js
+++ JavaScript Interview Guide/1. Promise.all() Polyfill.js
@@ -22,7 +22,61 @@ function createRejectPromise(time, promiseNumber) {
 
 try {
   const res = await Promise.all([createResolvePromise(100, 1), createResolvePromise(200, 2)]);
-  console.log("Response: ", res);
+  console.log("Response 1: ", res);
 } catch (err) {
-  console.error("Error: ", err);
-}
+  console.error("Error 1: ", err);
+};
+
+
+// Promise.all() Polyfill Using .then().catch()
+function promiseAllPolyfill(promiseArray) {
+  let results = [], promisesCompleted = 0;
+  return new Promise((resolve, reject) => {
+    promiseArray.forEach((promise, index) => {
+      promise
+        .then((result) => {
+          results[index] = result;
+          promisesCompleted += 1;
+          if(promisesCompleted === promiseArray.length) {
+            resolve(results);
+          }
+        })
+        .catch((error) => {
+          reject(error);
+        })
+    });
+  });
+};
+
+promiseAllPolyfill([createResolvePromise(100, 1), createResolvePromise(200, 2)])
+  .then((res) => console.log("Response 2: ", res))
+  .catch((err) => console.error("Error 2: ", err));
+
+
+
+
+// Promise.all() Polyfill Using async/await
+function promiseAllPolyfill(promiseArray) {
+  let results = [], promisesCompleted = 0;
+  return new Promise((resolve, reject) => {
+    promiseArray.forEach(async(promise, index) => {
+      try {
+        let result = await Promise.resolve(promise);
+        results[index] = result;
+        promisesCompleted += 1;
+        if(promisesCompleted === results.length) {
+          resolve(results);
+        }
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+};
+
+try {
+  const res = await promiseAllPolyfill([createResolvePromise(100, 1), createResolvePromise(200, 2)]);
+  console.log("Response 3: ", res);
+} catch (err) {
+  console.error("Error 3: ", err);
+};
